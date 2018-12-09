b97b357abb22eb5e79639fe369e41eb8eb01d2e4
davidcraig <davidcraig1986@gmail.com>
2018-12-09 22:26:46 +0000

    Aspect of the Monkey (Hunter).

--- data/classes/hunter/bm/AspectOfTheMonkey.js
+++ data/classes/hunter/bm/AspectOfTheMonkey.js
@@ -1,6 +1,8 @@
 export const AspectOfTheMonkey = {
+  name: "Aspect of the Monkey",
   firstLearnable: 4,
   hasRanks: false,
   cost: 100,
-  school: 'Beast Mastery'
+  school: 'Beast Mastery',
+  type: 'Self Buff'
 }
